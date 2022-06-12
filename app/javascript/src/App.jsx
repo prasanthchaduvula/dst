aa3a70845ac0231bf0ca4d9ae316e42f9071b067
prasanthchaduvula <chaduvulaprasanth1998@gmail.com>
2022-06-12 20:19:18 +0530

    Intial setup

---
+++ app/javascript/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+
+const App = () => {
+  return <>
+  <h1>Hello react</h1>
+  <Button variant="contained">Hello World</Button>
+
+  </> 
+}
+
+export default App
